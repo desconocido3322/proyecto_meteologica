3b2e274ece75f0df9a941d057da5664f4dd43e57
desconocido3322 <130305280+desconocido3322@users.noreply.github.com>
2024-11-23 01:03:14 -0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras import metrics
+import tensorflow as tf
 import sys
 
 # Configuración de semillas para reproducibilidad
